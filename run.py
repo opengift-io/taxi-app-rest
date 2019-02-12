5a3158f1afa067ac9e0613159be59f3ea276057d
gvammer <gvamm3r@gmail.com>
2019-02-12 18:24:00 +0300

    0.2.0

--- run.py
+++ run.py
@@ -6,6 +6,8 @@ from eve.auth import BasicAuth
 from settings import ADMIN_USERNAME, ADMIN_PASSWORD
 from eve.auth import requires_auth
 from eve.methods.post import post_internal
+from eve.methods.put import put_internal
+from eve.methods.common import get_document
 import json
 
 class MyBasicAuth(BasicAuth):
@@ -17,10 +19,40 @@ def get_distance_price(distance):
     return 100 * (float(distance) / 1000.0)
 
 
+def before_update(resource_name, item, payload):
+
+    if resource_name == 'drivings':
+        driving = json.loads(item.get_data())
+
+        if 'driver' in driving and driving['driver'] == 'CLEAN':
+            original = get_document(resource_name, False, **{"_id": payload['_id']})
+            new = {}
+            for k in original:
+                new[k] = original[k]
+
+            new['customer'] = str(new['customer'])
+            del new['_created']
+            del new['driver']
+            del new['_updated']
+            del new['_etag']
+            del new['_id']
+
+            del driving['driver']
+
+            for k in driving:
+                new[k] = driving[k]
+
+            return put_internal(resource_name, new, False, True, **{"_id": payload['_id']})
+            # app.data.replace(resource_name, original['_etag'], new, original)
+
+
+
+
 def after_insert(resource_name, request, payload):
     if resource_name == 'drivings':
         for doc in [request.get_data()]:
             doc = json.loads(doc)
+
             if doc['destinations']:
                 destinations = json.loads(doc['destinations'])
 
@@ -47,6 +79,8 @@ def before_insert(resource_name, documents):
 app = Eve(auth=MyBasicAuth)
 app.on_insert += before_insert
 app.on_post_POST += after_insert
+app.on_pre_PATCH += before_update
+
 
 
 @app.route('/get_price')
